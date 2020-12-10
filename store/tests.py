17320992c208f9fc184bf04464be2ef46821b546
AlGifari <jihar@tlab.co.id>
2020-12-10 16:09:54 +0700

    finishing api detail and get all

--- store/tests.py
+++ store/tests.py
@@ -1,3 +1,49 @@
-from django.test import TestCase
+"""
+This module is used to test store
+"""
+import uuid
+from django.utils import timezone
+from django.test import TestCase, Client
+from django.urls import reverse, resolve
 
+from .models import Store
 # Create your tests here.
+
+"""
+StoreTestCase is used to test
+"""
+
+
+class StoreTestCase(TestCase):
+    def setUp(self):
+        Store.objects.create(
+            owner_id=uuid.uuid4(),
+            name="kancio shop",
+            phone_number="089020394003",
+            is_active=True,
+            priority=1,
+            image_url="img.png",
+            description="kancio store",
+            latitude=10,
+            longitude=20,
+            total_visitor=100,
+            created_at=timezone.now(),
+            updated_at=timezone.now(),
+            created_by=uuid.uuid4(),
+            updated_by=uuid.uuid4(),
+        )
+
+    def test_get(self):
+        mdl = Store.objects.get(name="kancio shop")
+        print(mdl.name)
+
+
+class StoreClientTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_list(self):
+        store_list = reverse('store:list')
+        uri = resolve(store_list)
+        data = self.client.get(uri.route)
+        print(data)
--- store/views.py
+++ store/views.py
@@ -1,11 +1,31 @@
-from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 
 from .models import Store
-from .serializers import StoreSerializer
 
 
-# Create your views here.
-class StoreVieSet(viewsets.ModelViewSet):
-    queryset = Store.objects.all().order_by('created_at')
-    serializer_class = StoreSerializer
+class StoreAPI(object):
+    @api_view(["GET"])
+    def get_all(request):
+        resp = Store.objects.all().values()
+        json_resp = {
+            "data": resp
+        }
+        return Response(json_resp)
+
+    @api_view(["GET"])
+    def get_detail(request, id):
+        try:
+            data = Store.objects.get(id=id)
+            json_resp = {
+                "data": data,
+            }
+            return Response(json_resp)
+        except Store.DoesNotExist as err:
+            print(err)
+            json_resp = {
+                "code": status.HTTP_204_NO_CONTENT,
+                "message": f"{err} Store id is {id}",
+            }
+            return Response(json_resp, status=status.HTTP_204_NO_CONTENT)
