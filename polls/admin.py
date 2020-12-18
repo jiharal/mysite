7ba7712142734367c504e19dc5f30d0c7d0b8505
AlGifari <jihar@tlab.co.id>
2020-12-18 18:17:00 +0700

    update admin

--- polls/admin.py
+++ polls/admin.py
@@ -1,4 +1,22 @@
 from django.contrib import admin
 from . import models
 
-admin.site.register([models.Question,models.Choice,])
+
+class ChoiceInline(admin.TabularInline):
+    model = models.Choice
+    extra = 4
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_filter = ['pub_date']
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {
+            'fields': ['pub_date'],
+            'classes': ['collapse']
+        }),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(models.Question, QuestionAdmin)
--- polls/models.py
+++ polls/models.py
@@ -17,6 +17,10 @@ class Question(models.Model):
         print(self.pub_date)
         print(now)
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 
 class Choice(models.Model):
